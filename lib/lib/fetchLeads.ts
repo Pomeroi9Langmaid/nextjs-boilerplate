86a4b56714a660444bb3637171042b4b26900286
Pomeroi9Langmaid <langmaid.andrew@gmail.com>
2025-07-05 17:32:07 +0200

    Create fetchLeads.ts

---
+++ lib/lib/fetchLeads.ts
@@ -0,0 +1,12 @@
+import { supabase } from './supabaseClient';
+
+export async function fetchLeads() {
+  const { data, error } = await supabase.from('leads').select('*');
+
+  if (error) {
+    console.error('Error fetching leads:', error.message);
+    return [];
+  }
+
+  return data || [];
+}
