2022eba572555bbad8a497a3e3de54c32a272f64
Pomeroi9Langmaid <langmaid.andrew@gmail.com>
2025-07-05 17:42:00 +0200

    Update fetchLeads.ts

--- lib/lib/fetchLeads.ts
+++ lib/lib/fetchLeads.ts
@@ -3,7 +3,7 @@ import { supabase } from './supabaseClient'
 export async function fetchLeads() {
   const { data, error } = await supabase.from('leads').select('*')
   if (error) {
-    console.error('Error fetching leads:', error)
+    console.error('Error fetching leads:', error.message)
     return []
   }
   return data
